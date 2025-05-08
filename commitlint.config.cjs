486b2e9e6cfc17489f78e05b293757c678fb276b
phamtuan19hd <phamtuan19hd@gmail.com>
2025-05-08 10:13:53 +0700

    test commit lint and eslint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -7,6 +7,6 @@
 module.exports = {
    extends: ['@commitlint/config-conventional'],
    rules: {
-      'type-enum': [2, 'always', ['feat', 'fix', 'refactor']],
+      'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'chore', 'docs']],
    },
 };
